2d3623f064035ffb4409190a5eb4eed537a93293
Song-Q <35965259+Song-Q@users.noreply.github.com>
2019-05-03 21:53:14 +0800

    Add files via upload

--- def_func.py
+++ def_func.py
@@ -29,3 +29,45 @@ def quadratic(a, b, c):
         return x1, x2
     else:
         print('The unary quadratic equation has no real number solution')
+        
+
+#设置默认参数（必选参数在前，默认参数在后），默认参数必须指向不变对象
+def enroll(name, gender, age=6, city='Beijing'):
+    print('name:', name)
+    print('gender:', gender)
+    print('age:', age)
+    print('city', city)
+    
+
+#可变参数（在参数前面加了一个*号）
+def calc(*numbers):
+    sum = 0
+    for n in numbers:
+        sum = sum + n*n
+    return sum
+
+calc(1, 2)
+#5
+nums = [1, 2, 3]
+calc(*nums)
+
+#关键字参数(允许传入任意个含参数名的参数，都会在函数内被组装为一个dict)
+def person(name, age, **kw):
+    print('name:', name, 'age:', age, 'other:', kw)
+
+person('Adam', 45, gender='M'm job='Engineer')
+#name: Adam age: 45 other: {'gender': 'M', 'job': 'Engineer'}
+extra = {'city': 'Beijing', 'job':'Engineer'}
+person('Jack', 24, city=extra['city'], job=extra['job'])
+#name: Jack age: 24 other: {'city': 'Beijing', 'job': 'Engineer'}
+person('Jack', 24, **extra)
+
+#命名关键字参数(命名关键字参数需要一个特殊分隔符*，*后面
+#的参数被视为命名关键字参数)
+def person(name, age, *, city, job):
+    print(name, age, city, job)
+#命名关键字参数必须传入参数名
+person('Jack', 24, city='Beijing', job='Engineer')
+
+#*args是可变参数，args 接收的是一个tuple
+#**kw是关键字参数，kw 接收的是一个dict
